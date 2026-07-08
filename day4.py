3466ef9a8aaa15947181549e767cb98b3affafc2
Nataly Caicedo <nataliecaicedor29@gmail.com>
2026-07-08 12:45:31 -0500

    Día 5: bucles for y while

---
+++ day4.py
@@ -0,0 +1,18 @@
+#For, while, break y continue
+#Loops
+fruits = ["mango", "banana","apple"]
+
+for fruit in fruits:
+    print(f"I like the {fruit}")
+
+for i in range(1, 6):
+    print(f"Semana {i} del plan")
+
+#Menu 
+while True:
+    opcion = input("1. Saludar 2. Salir")
+    if opcion == "1":
+        print("Hello!")
+    elif opcion == "2":
+        print("Good bye!")
+        break
